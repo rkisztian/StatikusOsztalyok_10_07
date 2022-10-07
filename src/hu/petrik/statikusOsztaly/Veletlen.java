b629670810323f7c8053becf5ef1db6d09cd8786
John Doe <johndoe@example.com>
2022-10-07 16:33:33 +0200

    Már csak házi feladat van

--- src/hu/petrik/statikusOsztaly/Veletlen.java
+++ src/hu/petrik/statikusOsztaly/Veletlen.java
@@ -13,9 +13,9 @@ public final class Veletlen {
 
         private static final Random rnd = new Random();
 
-        private static final List<String> VEZ_nev = feltolt("file/veznev.txt");
-        private static final List<String> NOI_KERESZT_nev = feltolt("file/noikernev.txt");
-        private static final List<String> FERFI_KERESZT_nev = feltolt("file/ferfikernev.txt");
+        private static final List<String> VEZ_nev = feltolt("files/veznev.txt");
+        private static final List<String> NOI_KERESZT_nev = feltolt("files/noikernev.txt");
+        private static final List<String> FERFI_KERESZT_nev = feltolt("files/ferfikernev.txt");
 
         private static List<String> feltolt(String fajlnev){
             List<String> lista = new ArrayList<>();
@@ -38,5 +38,49 @@ public final class Veletlen {
             }
             return rnd.nextInt(max - min + 1) + min;
         }
+
+        public static char velKarakter(char min, char max){
+            return (char) velEgesz(min, max);
+        }
+
+        public static String velVezeteknev(){
+            return VEZ_nev.get(rnd.nextInt(VEZ_nev.size()));
+        }
+
+
+    /**
+     * Véletlen keresztnevet generál a megadott paraméter alapján.
+     * Paraméter értéktől függően férfi vagy női keresztnevet
+     * @param nem A generált név neme. Igaz, ha férfi, Hamis, ha nő
+     * @return Generált keresztnév.
+     */
+    public static String velKeresztnev(boolean nem){
+        String keresztnev;
+        if(nem){
+            keresztnev = velFerfiKeresztnev();
+            }else {
+            keresztnev = velNoiKeresztnev();
+        }
+        return keresztnev;
+        }
+
+    private static String velNoiKeresztnev() {
+        return  NOI_KERESZT_nev.get(rnd.nextInt(NOI_KERESZT_nev.size()));
+    }
+
+    private static String velFerfiKeresztnev() {
+        return FERFI_KERESZT_nev.get(rnd.nextInt(FERFI_KERESZT_nev.size()));
+    }
+
+
+    /**
+     * Véletlen nevet generál a megadott paraméter alapján.
+     * Paraméter értéktől függően férfi vagy női nevet
+     * @param nem A generált név neme. Igaz, ha férfi, Hamis, ha nő
+     * @return Generált nev.
+     */
+    public static String velTeljesNev(boolean nem){
+        return velVezeteknev() + " " + velKeresztnev(nem);
+    }
 
 }
